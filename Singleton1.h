2a6faf0823022573691ef57b15069586ed24526e
Mikhail Myasnikov <myasnikov.mike@gmail.com>
2015-03-02 16:47:42 +0500

    Update Singleton1.h

--- Singleton1.h
+++ Singleton1.h
@@ -1,1 +1,20 @@
 
+// Singleton1.h
+class Singleton
+{
+private:
+static Singleton * p_instance;
+// Конструкторы и оператор присваивания недоступны клиентам
+Singleton() {}
+Singleton(const Singleton&);
+Singleton& operator=(Singleton&);
+public:
+static Singleton * getInstance() {
+if (!p_instance)
+p_instance = new Singleton();
+return p_instance;
+}
+};
+extern "C" {
+__declspec(dllexport) Singleton* getFunctionModuleObject();
+}
